496c2a8a84a0156d23f937451f0570963644119c
axolotl-inc <axolotl-inc@penguin>
2021-05-01 19:47:15 -0700

    delleted unused stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 pub mod structs;
 pub mod tell;
 pub mod readinput;
-pub mod read_file;
 
 fn main() {
     let mut axolotl_vec = structs::AxolotlVec::new();
     tell::intro();
     tell::help();
     readinput::handle_input(&mut axolotl_vec);
-    read_file::read_save(String::from("test.txt"), axolotl_vec);
 }
